5cea33a05dd144c4c1a7e4b3de125af789f2b95b
goneill245 <50212118+goneill245@users.noreply.github.com>
2019-05-28 11:07:23 -0700

    areaofcircle.c

---
+++ areaofcircle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float areaofcircle;
+(float radius)
+{
+printf ("radius: %f\n", radius);
+
+return (radius *radius *3.14);
+}
+
+int main() {
+float area;
+
+for (float i = 3.5; i <= 12.5, i=i+1)
+{
+area= areaofacircle (i);
+printf("area: %f\n", area);
+
+}
+}
