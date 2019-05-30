1119aff11e13f278bb4cc19fa7abc1bf1f1317ac
goneill245 <50212118+goneill245@users.noreply.github.com>
2019-05-29 22:04:53 -0700

    student.c

---
+++ student.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+
+
+
+void printStudent (struct Student* student)
+
+
+{
+
+
+ printf("Student First Name: %s\nStudent Last Name: %s\nStudent Age: %d\nStudent ID Number: %d\n", student->firstname, student->lastname,name, student->age, student->id);
+
+
+}
